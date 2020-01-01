572f225e55f7a9d9bf65de11d9537255a068b3c0
SALMAN <elfadili.salman@gmail.com>
2020-01-01 23:13:58 +0100

    first commit

---
+++ src/main/java/com/salman/himry/HimryApplication.java
@@ -0,0 +1,13 @@
+package com.salman.himry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HimryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HimryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salman/himry/HimryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salman.himry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HimryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
